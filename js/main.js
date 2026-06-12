a6a7b11011301c1c2a7e369f692225080457259c
Natka2091 <fomnata41@gmail.com>
2026-06-12 21:09:55 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const advert = [
+{
+    autor: {avatar: 'img/avatars/user03.png'},
+    offer: {
+        title: 'Apartment in the city center',
+        address: '35.67891, 139.75432',
+        price: 25000,
+        type: 'flat',
+        rooms: 3,
+        guests: 5,
+        checkin: '14:00',
+        checkout: '12:00',
+        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+        description: 'Bright apartment near the metro',
+        photos: 'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    },
+    location: {
+      x: 35.67891,
+      y: 139.75432
+    }
+},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+];
