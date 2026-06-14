dc9e97d9875aed1b8fc1f7b104ca24166324b470
Natka2091 <fomnata41@gmail.com>
2026-06-15 00:29:11 +0300

    'Project'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
+
+const types = ['palace','flat','house','bungalow']
+const minX = 35.65000;
+const maxX = 35.70000;
+const minY = 139.70000;
+const maxY = 139.80000;
+const x = Math.random() * (maxX - minX + 1) + minX;
+const y = Math.random() * (maxY - minY + 1) + minY;
+
+function getAvatarUrl(number){
+    return `img/avatars/user0${number}.png`
+}
+
+function getRandomCoordinates(){
+    return {
+        x: Number(x.toFixed(5)),
+        y: Number(y.toFixed(5))
+    };
+}
+
+function getRandomNumber(max){
+   return  Math.floor(Math.random() * max); 
+}
+
+function getRandomElementFromArray(array){
+    const randomNumber =getRandomNumber(array.length)
+    
+ return array[randomNumber]
+}
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const offers = new Array(10).fill(null).map((el,index)=>{
+    return {
+        author: {
+        avatar: getAvatarUrl(index+1)
+        },
+        offer: {
+            title:  'Nice Apartment',
+            address: getRandomCoordinates(),
+            rooms: getRandomInt(1, 4),
+            type: getRandomElementFromArray(types),
+        },
+    }
+    
+})
+console.log(offers)
 const advert = [
 {
     autor: {avatar: 'img/avatars/user03.png'},
