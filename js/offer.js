47f74c1fe114b16908fe11dba005bf92ba19bc22
Natka2091 <fomnata41@gmail.com>
2026-06-19 14:31:49 +0300

    'interim result'

--- js/offer.js
+++ js/offer.js
@@ -1,11 +1,40 @@
 import { offers } from "./data";
 
-function createOffer(offer){
-1 пойти и найти елемент с айди кард
-2 клонируем елемент со всеми внутреними елементами
-3 для склоненого елемента меняем значения на те которые в оффере
+function createCard(offerData){
+const template = document.querySelector('#card').content;
+const clone = template.querySelector('.popup').cloneNode(true);
+const mapCanvas = document.querySelector('#map-canvas')
+console.log(mapCanvas);
+
+const avatarEl = clone.querySelector('.popup__avatar')
+avatarEl.src = offerData.author.avatar;
+const titleEl = clone.querySelector('.popup__title')
+titleEl.textContent = offerData.offer.title;
+const addresEl = clone.querySelector('.popup__text--address')
+addresEl.textContent = offerData.offer.address;
+const priceEl = clone.querySelector('.popup__text--price')
+priceEl.textContent = `${offerData.offer.price} ₽/ночь`;
+const typeEl = clone.querySelector('.popup__type')
+typeEl.textContent = offerData.offer.type;
+const roomsGuestsEl = clone.querySelector('.popup__text--capacity')
+roomsGuestsEl.textContent = `${offerData.offer.rooms} комнаты для ${offerData.offer.guests} гостей`
+const checkinCheckoutEl = clone.querySelector('.popup__text--time')
+checkinCheckoutEl.textContent = `Заезд после ${offerData.offer.checkin}, выезд до ${offerData.offer.checkout}`;
+
+const featuresEl = clone.querySelector('.popup__features')
+const featureList = clone.querySelectorAll('.popup__feature')
+const textArray = Array.from(featureList).map(item => item.textContent)
+
+const descriptionEl = clone.querySelector('.popup__description')
+descriptionEl.textContent = offerData.offer.description;
+const photosEl = clone.querySelector('.popup__photos')
+
+mapCanvas.append(clone);
+
 }
 
-offers.forEach((el)=>{
-    createOffer(el)
+
+offers.forEach((offer)=>{
+
+   createCard(offer)
 })
